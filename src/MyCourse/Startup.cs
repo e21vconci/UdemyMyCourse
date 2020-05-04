bd88677210805a842524a392185e7078bc0009c5
encou <enzo_con@hotmail.it>
2020-05-04 18:09:59 +0200

    Aggiunto ASP.NET Core MVC

--- src/MyCourse/Startup.cs
+++ src/MyCourse/Startup.cs
@@ -15,9 +15,11 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Qui vengono inseriti i middleware
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsProduction()) 
@@ -32,10 +34,13 @@ namespace MyCourse
 
             app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            // Middleware di routing
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routeBuilder =>
             {
-                string nome = context.Request.Query["nome"];
-                await context.Response.WriteAsync($"Hello {nome}!");
+                // es. /courses/detail/5
+                // route di default
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
