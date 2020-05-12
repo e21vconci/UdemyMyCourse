18d509e81d3f936099296a04f6892f12981d9485
Enzo <enzocon91@gmail.com>
2020-05-12 12:36:12 +0200

    Aggiunta interfaccia ICourseService

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -7,17 +7,23 @@ namespace MyCourse.Controllers
 {
     public class CoursesController : Controller
     {
-        public IActionResult Index() 
+        private readonly ICourseService courseService;
+        public CoursesController(ICourseService courseService)
+        {
+            this.courseService = courseService;
+        }
+
+        public IActionResult Index()
         {
             ViewData["Title"] = "Catalogo dei corsi";
-            var courseService = new CourseService();
+            //var courseService = new CourseService();
             List<CourseViewModel> courses = courseService.GetCourses();
             return View(courses);
         }
 
         public IActionResult Detail(int id)
         {
-            var courseService = new CourseService();
+            //var courseService = new CourseService();
             CourseDetailViewModel viewModel = courseService.GetCourse(id);
             ViewData["Title"] = viewModel.Title;
             return View(viewModel);
