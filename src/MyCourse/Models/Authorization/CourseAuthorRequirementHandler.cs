40c8fafa4df54c132d72cec2e3507a832214c9a0
Enzo <enzocon91@gmail.com>
2021-06-18 13:02:27 +0200

    Se non ho id nell'url lo recupero dal context

--- src/MyCourse/Models/Authorization/CourseAuthorRequirementHandler.cs
+++ src/MyCourse/Models/Authorization/CourseAuthorRequirementHandler.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using MyCourse.Models.Services.Application.Courses;
+using MyCourse.Models.Services.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,10 +28,19 @@ namespace MyCourse.Models.Authorization
             // 1.leggere l’id dell’utente dalla sua identità
             string userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            // 2.capire a quale corso sta cercando di accedere (/Courses/Edit/1)
-            int courseId =  Convert.ToInt32(httpContextAccessor.HttpContext.Request.RouteValues["id"]);
+            // 2.capire a quale corso sta cercando di accedere dall'url della richiesta (/Courses/Edit/1) 
+            int courseId = context.Resource is int ? (int)context.Resource : // Se non ho l'id nell'url
+                           Convert.ToInt32(httpContextAccessor.HttpContext.Request.RouteValues["id"]);
+
+            if (courseId == 0)
+            {
+                context.Fail();
+                return;
+            }
 
             // 3.estrarre dal database l’id dell’autore del corso
+            // per evitare query al db potrei utilizzare un claim personalizzato per memorizzare nel cookie di autenticazione
+            // gli id dei corsi appartenenti al docente
             string authorId = await courseService.GetCourseAuthorIdAsync(courseId);
 
             // 4.verificare che l’id dell’utente sia uguale all’id dell’autore del corso
