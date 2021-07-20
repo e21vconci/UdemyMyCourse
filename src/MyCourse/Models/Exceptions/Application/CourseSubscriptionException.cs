badef66531485b0065298d8aedacd2cea0dfac3f
Enzo <enzocon91@gmail.com>
2021-07-20 17:26:10 +0200

    Eccezione personalizzata per errore iscrizione

---
+++ src/MyCourse/Models/Exceptions/Application/CourseSubscriptionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCourse.Models.Exceptions.Application
+{
+    public class CourseSubscriptionException : Exception
+    {
+        public CourseSubscriptionException(int courseId) : base($"Could not subscribe to course {courseId}")
+        {
+
+        }
+    }
+}
